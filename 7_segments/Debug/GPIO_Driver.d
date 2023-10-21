0142406c0a630b85cd6696048b43874c36f13c0e
emmadhammdy <Emadhamdy1qew@gmail.com>
2023-10-21 21:36:26 +0300

    remove 7_Seg

--- 7_segments/Debug/GPIO_Driver.d
+++
@@ -1,6 +0,0 @@
-GPIO_Driver.o GPIO_Driver.o: ../GPIO_Driver.c ../GPIO_Driver.h \
-  ../platformtypes.h
-
-../GPIO_Driver.h:
-
-../platformtypes.h:
--- 7_segments/Debug/main.d
+++
@@ -1,5 +0,0 @@
-main.o main.o: ../main.c ../segment_7.h ../platformtypes.h
-
-../segment_7.h:
-
-../platformtypes.h:
