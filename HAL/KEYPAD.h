11bcbc58b562bb2d62cbad4f719e86bce2b2b281
emmadhammdy <Emadhamdy1qew@gmail.com>
2023-10-21 23:29:46 +0300

    KEYPAD DRIVER

---
+++ HAL/KEYPAD.h
@@ -0,0 +1,20 @@
+/*
+ * KEYPAD.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: Emad Hamdy
+ */
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+
+#include "platformtypes.h"
+
+
+void keypad_cfg();
+
+usint8_t keypad_read(usint8_t num);
+
+
+#endif /* KEYPAD_H_ */
