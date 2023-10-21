cc1a86b412b3ccead4ce5e41a3159f10db359bd3
emmadhammdy <Emadhamdy1qew@gmail.com>
2023-10-21 21:47:13 +0300

    Hardware Abstraction Layer

---
+++ HAL/Debug/main.d
@@ -0,0 +1,5 @@
+main.o main.o: ../main.c ../segment_7.h ../platformtypes.h
+
+../segment_7.h:
+
+../platformtypes.h:
---
+++ HAL/Debug/segement_7.d
@@ -0,0 +1,8 @@
+segement_7.o segement_7.o: ../segement_7.c ../segment_7.h \
+  ../platformtypes.h ../GPIO_Driver.h
+
+../segment_7.h:
+
+../platformtypes.h:
+
+../GPIO_Driver.h:
