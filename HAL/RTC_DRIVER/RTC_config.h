315ec24e3fad2449ff3685382f25abc9b57e941e
emmadhammdy <Emadhamdy1qew@gmail.com>
2023-11-04 13:32:46 +0200

    Real Time Clock Driver

---
+++ HAL/RTC_DRIVER/RTC_config.h
@@ -0,0 +1,13 @@
+/*******************************************************************************/
+/*   Author    : EMAD HAMDY                                                    */
+/*   Version   : V01                                                           */
+/*   Date      : 3 November 2023                                               */
+/*   Logs      : V01 : Initial creation                                        */
+/*******************************************************************************/
+
+#ifndef RTC_CONFIG_H_
+#define RTC_CONFIG_H_
+
+
+
+#endif /* RTC_CONFIG_H_ */
