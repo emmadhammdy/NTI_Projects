7ea63a3a742478e29bb28f94e53ef871429a12c6
emmadhammdy <Emadhamdy1qew@gmail.com>
2023-11-14 11:59:27 +0200

    Dc MOTOR Driver

---
+++ HAL/DC_Motor Driver/DC_MOTOR_config.h
@@ -0,0 +1,16 @@
+/*******************************************************************************/
+/*   Author    : EMAD HAMDY                                                    */
+/*   Version   : V01                                                           */
+/*   Date      : 11 November 2023                                               */
+/*   Logs      : V01 : Initial creation                                        */
+/*******************************************************************************/
+
+
+#ifndef DC_MOTOR_CONFIG_H_
+#define DC_MOTOR_CONFIG_H_
+
+
+
+
+
+#endif /* DC_MOTOR_CONFIG_H_ */
---
+++ HAL/DC_Motor Driver/DC_MOTOR_private.h
@@ -0,0 +1,13 @@
+/*******************************************************************************/
+/*   Author    : EMAD HAMDY                                                    */
+/*   Version   : V01                                                           */
+/*   Date      : 11 November 2023                                               */
+/*   Logs      : V01 : Initial creation                                        */
+/*******************************************************************************/
+
+#ifndef DC_MOTOR_PRIVATE_H_
+#define DC_MOTOR_PRIVATE_H_
+
+
+
+#endif /* DC_MOTOR_PRIVATE_H_ */
